e5138c9fd896ba81f4b67a07eec9e1d8c13987d5
Andrew Duong <duongsters@gmail.com>
2019-10-21 14:05:23 -0700

    fixed routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-require(".routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
